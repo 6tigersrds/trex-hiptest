ffd35bd4abc59e64dd2b4d69ee71e0ec83f322b0
R S <6tigersrds@gmail.com>
2020-04-12 00:01:21 +0000

    initial commit

---
+++ src/main/webapp/app/admin/logs/logs.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HiptestSharedModule } from 'app/shared/shared.module';
+
+import { LogsComponent } from './logs.component';
+
+import { logsRoute } from './logs.route';
+
+@NgModule({
+  imports: [HiptestSharedModule, RouterModule.forChild([logsRoute])],
+  declarations: [LogsComponent]
+})
+export class LogsModule {}
